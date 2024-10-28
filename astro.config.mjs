0f02dc53908d8905cc6ccb6e89bab64b530c30b4
Yazid Talbi <yazidtalbi@MacBook-Pro-de-Yazid.local>
2024-10-28 08:06:48 +0100

    vercel setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://yazidtalbi.com",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [tailwind(), react(), mdx(), sitemap()],
 });
