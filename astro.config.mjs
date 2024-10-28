74fd656ac8318c34efe63b01bf76904f66423ed0
Yazid Talbi <yazidtalbi@MacBook-Pro-de-Yazid.local>
2024-10-28 08:16:09 +0100

    fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   site: "https://yazidtalbi.com",
-  output: "server",
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
